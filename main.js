8527e4d8ff7d4ce373cafc4038c77e6320f205da
Raja Kapur <raja.kapur@gmail.com>
2012-10-07 14:27:01 -0300

    Added support for php functions and whitespaces
    Previously the cursor could not be in the middle of the line or the extension would error out saying function not found.
    
    The getTarget() function now checks from the start of the line, and generateComment() now prefixes the comment block with the same whitespace spaces or tabs as the function def line used

--- main.js
+++ main.js
@@ -45,18 +45,37 @@ define(function (require, exports, module) {
         
         var editor = EditorManager.getCurrentFullEditor();
         var pos = editor.getCursorPos();
-        
+        pos.ch = 0;       
+ 
         editor._codeMirror.replaceRange(input, pos);
 
-        EditorManager.focusEditor();        
+        EditorManager.focusEditor();
+        
     }
-
+    
+    /**
+     * get the whitespace characters from line start to beginning of function def
+     * @param string input lines from start of the function definition
+     * @param string match function definition start
+     */
+    function getPrefix(input, match) {
+        
+        var indexOf = input.indexOf(match),
+            prefix = "";
+        if (indexOf != -1) {
+            prefix = input.substr(0, indexOf);
+        }
+        
+        return prefix;
+        
+    }
     
     function getTarget() {
         
         var editor = EditorManager.getCurrentFullEditor();
         var pos = editor.getCursorPos();
-        
+        pos.ch = 0;       
+ 
         // Take the text of the document, starting with the current cursor line
         var txtFrom = editor._codeMirror.getRange(pos, {line: editor._codeMirror.lineCount() });
         
@@ -72,13 +91,36 @@ define(function (require, exports, module) {
         if (results[0] === "function") {
             return {
                 name: results[1],
-                params: results.slice(2)
+                params: results.slice(2),
+                prefix: getPrefix(txtFrom, results[0])
             };
         } 
-        else if (results[0] === "var" && results[2] === "function") { 
+        else if (results[0] === "private" && results[1] === "function") { 
             return {
                 name: results[1],
-                params: results.slice(3)
+                params: results.slice(3),
+                prefix: getPrefix(txtFrom, results[0])
+            };            
+        }
+        else if (results[0] === "public" && results[1] === "function") { 
+            return {
+                name: results[1],
+                params: results.slice(3),
+                prefix: getPrefix(txtFrom, results[0])
+            };            
+        }
+        else if (results[0] === "static" && results[1] === "function") { 
+            return {
+                name: results[1],
+                params: results.slice(3),
+                prefix: getPrefix(txtFrom, results[0])
+            };            
+        }
+        else if (results[0] === "var" && results[1] === "function") { 
+            return {
+                name: results[1],
+                params: results.slice(3),
+                prefix: getPrefix(txtFrom, results[0])
             };            
         }
         else {
@@ -88,32 +130,39 @@ define(function (require, exports, module) {
     }
     
     
-    function generateComment(fname, params) {
+    /**
+     * Generate comment block
+     * @param string fname function name
+     * @param string params function parameters
+     * @param string prefix whitespace prefix for comment block lines
+     */
+    function generateComment(fname, params, prefix) {
         
-        var output = "/**\n";
+        var output = [];
+        output.push("/**");
                 
         // Assume function is private if it starts with an underscore
         if (fname.charAt(0) === "_") {
-            output += " * @private\n";
+            output.push(" * @private");
         }
         
         // Add description
-        output += " * Description\n";
+        output.push(" * Description");
         
         // Add parameters
         if (params.length > 0) {
             var i;
             for (i = 0; i < params.length; i++) {
                 var param = params[i];
-                output += " * @param {type} " + param + " Description\n";
+                output.push(" * @param {type} " + param + " Description");
             }
         }
         
         // TODO use if 'return' is found in the function body?
         //output += " * @return {type} ???\n";
-        output += " */\n";
+        output.push(" */");
         
-        return output;
+        return prefix + output.join("\n" + prefix) + "\n";
     }
 
     
@@ -127,7 +176,7 @@ define(function (require, exports, module) {
             return;
         }
         
-        var comment = generateComment(target.name, target.params);
+        var comment = generateComment(target.name, target.params, target.prefix);
         
         insert(comment);
     }
